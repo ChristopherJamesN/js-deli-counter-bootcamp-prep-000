20ca78e9b7b70f3c2473d741b5aad2aa7b95e5f2
Chris Nady <nady.christopher@gmail.com>
2017-08-25 20:30:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var katzDeliLine = [];
 function takeANumber(katzDeliLine, name) {
   var i = katzDeliLine.length;
   katzDeliLine[i] = name;
-  console.log("Welcome, " + name + ". You are number " + i + 1 + " in line.");
+  return ("Welcome, " + name + ". You are number " + i + 1 + " in line.");
 }
