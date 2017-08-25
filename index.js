b371c9936eefccfaf97cc755deb98207a024ac81
Chris Nady <nady.christopher@gmail.com>
2017-08-25 20:59:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function currentLine(line) {
     return "The line is currently empty.";
   } else {
     var i = 0;
-    var String arr = [];
+    String arr = [];
     for (i; i<line.length; i++) {
       arr[i] = (" " + (i+1) + ". " + line[i]);
     }
