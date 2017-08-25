4fba0ca845f7c74f6b0e8240ef09dada59b8f468
Chris Nady <nady.christopher@gmail.com>
2017-08-25 20:28:31 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,7 @@
+var katzDeliLine = [];
+
+function takeANumber(katzDeliLine, name) {
+  var i = katzDeliLine.length;
+  katzDeliLine[i] = name;
+  console.log("Welcome, " + name + ". You are number " + i + " in line.");
+}
