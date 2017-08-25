26db78704ed5eb36f4ebd6dbc3da6c0371bf4639
Chris Nady <nady.christopher@gmail.com>
2017-08-25 20:39:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 var katzDeliLine = [];
 
-function takeANumber(katzDeliLine, name) {
-  var i = katzDeliLine.length;
-  katzDeliLine[i] = name;
+function takeANumber(deliLine, name) {
+  var i = deliLine.length;
+  deliLine[i] = name;
   return ("Welcome, " + name + ". You are number " + (i+1) + " in line.");
 }
 
 function nowServing() {
-  if (katzDeliLine.length === 0) {
+  if (deliLine.length === 0) {
     return "There is nobody waiting to be served!";
   } else {
-    currentServing = ("Currently serving " + katzDeliLine[0] + ".");
-    katzDeliLine.unshift();
+    currentServing = ("Currently serving " + deliLine[0] + ".");
+    deliLine.unshift();
     return currentServing;
   }
 }
